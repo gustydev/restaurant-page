c3f4f91bcb9658e75f6bc0775d41e3f4a13d4353
gusty <gustavodiorlando@gmail.com>
2023-10-04 12:07:38 -0300

    Initiate tab switching logic + minor styling

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import loadHome from "./home.js";
 loadHome();
+
+const content = document.querySelector('div#content');
+const tabDiv = document.createElement('div');
+tabDiv.classList.add('tabs');
+content.appendChild(tabDiv);
+
+function newTab(tabText) {
+    const tab = document.createElement('button');
+    tab.textContent = tabText;
+    tab.id = tabText;
+    tab.addEventListener('click', () => {
+        console.log('do a thing'); // Replace later with content replacement
+    })
+    tabDiv.appendChild(tab);
+    
+};
+
+newTab('home');
+newTab('menu');
+newTab('contact');
