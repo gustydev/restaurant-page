d0510e13f48383669e1ad849e398b443d15e6968
gusty <gustavodiorlando@gmail.com>
2023-10-05 12:50:31 -0300

    Change contact page joke

--- src/contact.js
+++ src/contact.js
@@ -9,8 +9,7 @@ contactInfo.classList.add('info-card')
 
 contactInfo.innerHTML = `Restaurant Phone: <b>4002-8922</b> <br>
 Chef's Phone: <b>1234-5678</b> <br><br>
-<b>Location</b>: <br><iframe allowfullscreen="" class="map-top" height="450" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyD4iE2xVSpkLLOXoyqT-RuPwURN3ddScAI
-&amp;q=Eiffel+Tower,Paris+France" width="598"></iframe>`
+<b>Location</b>: Avenida Brigadeiro Faria Lima, 42069`
 
 const loadContact = () => {
     content.appendChild(headline);
